0e81654aab138ceb57c417408b7163dcad614bcf
kmtym1998 <kmtym.devloper@gmail.com>
2024-01-02 23:04:12 +0900

    LongestPullRequests はできてる

--- wrapper/pull_request.go
+++ wrapper/pull_request.go
@@ -2,6 +2,7 @@ package wrapper
 
 import (
 	"fmt"
+	"sort"
 	"time"
 
 	"github.com/kmtym1998/gh-wrapped/config"
@@ -17,28 +18,34 @@ type WrapResultPullRequest struct {
 	MergedCount int
 	// 当年に作成され、当年にマージされなかった、OPEN でない PR の数
 	ClosedCount int
-	// 作成 ~ マージまでが最も短かった PR
-	ShortestPullRequest SimplePullRequest
-	// 作成 ~ マージまでが最も長かった PR
-	LongestPullRequest SimplePullRequest
+	// 作成 ~ マージまでが最も短かった PR (上位 3 つ)
+	ShortestPullRequests []PullRequestDurationItem
+	// 作成 ~ マージまでが最も長かった PR (上位 3 つ)
+	LongestPullRequests []PullRequestDurationItem
 	// 作成 ~ マージまでの平均時間
 	AverageDuration time.Duration
-	// コメントが最も多かった PR
+	// コメントが最も多くつけられた PR
 	MostCommentedPullRequest SimplePullRequest
 	// コミットが最も多かった PR
 	MostCommitsPullRequest SimplePullRequest
 	// リポジトリごとに PR を出した数
-	SubmissionRanking []SubmissionRankingItem
+	SubmissionRanking []PullRequestSubmissionRankingItem
 	// 一番レビュー回数が多かったユーザー
 	MostReviewedBy string
 }
 
-type SubmissionRankingItem struct {
+type PullRequestDurationItem struct {
+	PullRequest SimplePullRequest
+	Duration    time.Duration
+}
+
+type PullRequestSubmissionRankingItem struct {
 	PullRequest SimplePullRequest
 	Count       int
 }
 
 type SimplePullRequest struct {
+	Title  string
 	Owner  string
 	Repo   string
 	Number int
@@ -84,7 +91,105 @@ func WrapPullRequest(repo repository.GitHubRepository, cfg *config.Config) (*Wra
 
 			return pr.State == repository.PullRequestStateClosed
 		}),
+		ShortestPullRequests: PickTopNPullRequestsDurationItem(
+			pullRequests,
+			func(pr1, pr2 *repository.PullRequest) bool {
+				if pr1.State != repository.PullRequestStateMerged {
+					return false
+				}
+
+				if !pr1.MergedAt.Valid {
+					return false
+				}
+
+				if pr2.State != repository.PullRequestStateMerged {
+					return false
+				}
+
+				if !pr2.MergedAt.Valid {
+					return false
+				}
+
+				sub1 := pr1.MergedAt.Time.Sub(pr1.CreatedAt)
+				sub2 := pr2.MergedAt.Time.Sub(pr2.CreatedAt)
+
+				return sub1 < sub2
+			},
+			3,
+		),
+		LongestPullRequests: func() []PullRequestDurationItem {
+			longestPR := lo.MaxBy(
+				pullRequests,
+				func(
+					pr *repository.PullRequest,
+					currentMax *repository.PullRequest,
+				) bool {
+					if pr.State != repository.PullRequestStateMerged {
+						return false
+					}
+
+					if !pr.MergedAt.Valid {
+						return false
+					}
+
+					newSub := pr.MergedAt.Time.Sub(pr.CreatedAt)
+					currentMaxSub := currentMax.MergedAt.Time.Sub(currentMax.CreatedAt)
+
+					return newSub > currentMaxSub
+				},
+			)
+
+			return []PullRequestDurationItem{{
+				PullRequest: SimplePullRequest{
+					Title:  longestPR.Title,
+					Owner:  longestPR.RepositoryOwner,
+					Repo:   longestPR.RepositoryName,
+					Number: longestPR.Number,
+					URL:    longestPR.URL,
+				},
+				Duration: longestPR.MergedAt.Time.Sub(longestPR.CreatedAt),
+			}}
+		}(),
 	}
 
 	return &result, nil
+}
+
+func PickTopNPullRequestsDurationItem(
+	list []*repository.PullRequest,
+	compareFunc func(a, b *repository.PullRequest) bool,
+	n int,
+) []PullRequestDurationItem {
+	if n < 1 {
+		panic("n must be greater than 0")
+	}
+
+	if compareFunc == nil {
+		panic("compareFunc must not be nil")
+	}
+
+	var copiedList []*repository.PullRequest
+	for _, pr := range list {
+		copiedList = append(copiedList, pr)
+	}
+
+	sort.SliceStable(copiedList, func(i, j int) bool {
+		return compareFunc(copiedList[i], copiedList[j])
+	})
+
+	var result []PullRequestDurationItem
+	for i := 0; i < n; i++ {
+		result = append(result, PullRequestDurationItem{
+			PullRequest: SimplePullRequest{
+				Title:  copiedList[i].Title,
+				Owner:  copiedList[i].RepositoryOwner,
+				Repo:   copiedList[i].RepositoryName,
+				Number: copiedList[i].Number,
+				URL:    copiedList[i].URL,
+			},
+			Duration: copiedList[i].MergedAt.Time.Sub(copiedList[i].CreatedAt),
+		})
+	}
+
+	return result
 }
